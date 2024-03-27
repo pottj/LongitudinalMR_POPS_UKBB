b60ab9541c7a1e1253c7d58a68ca5e8c27cbcd97
pottj <pott.ne.ja@gmail.com>
2024-03-27 09:22:35 +0000

    add script to generate plot of trajectories

---
+++ realdata/scripts/05_Evals_03_Trajectory_EFW.R
@@ -0,0 +1,85 @@
+#' ---
+#' title: "Get Trajectories"
+#' subtitle: "Longitudinal MVMR in POPS"
+#' author: "Janne Pott"
+#' date: "Last compiled on `r format(Sys.time(), '%d %B, %Y')`"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Introduction ####
+#' ***
+#' I want the trajectories for EFW (in kg) including the final birth weight, colored by eCS. 
+#' 
+#' # Initialize ####
+#' ***
+rm(list = ls())
+time0 = Sys.time()
+
+source("../../SourceFile_HPC.R")
+
+tag = format(Sys.time(), "%Y-%m-%d")
+tag = gsub("2024-","24-",tag)
+tag = gsub("-","",tag)
+
+#' # Load data ####
+#' ***
+myGD_files = list.files(path = POPS_phenotypes,pattern = "01_Prep_04")
+loaded1 = load(paste0(POPS_phenotypes,myGD_files[1]))
+loaded1
+loaded2 = load(paste0(POPS_phenotypes,myGD_files[2]))
+loaded2
+
+#' # Get plot data ####
+#' ***
+names(myTab_X)
+myTab_X =myTab_X[,c(1,2,4,5,6,18)]
+dummy = myTab_X[!is.na(efwcomb),.N,by=POPSID]
+myTab_X = myTab_X[POPSID %in% dummy[N>=2,POPSID]]
+
+names(myTab_Y)
+myTab_Y[,scan := 4]
+myTab_Y =myTab_Y[,c(1,9,18,55,10,12)]
+names(myTab_Y) = names(myTab_X)
+myTab_Y = myTab_Y[POPSID %in% myTab_X$POPSID]
+
+plotData= rbind(myTab_X,myTab_Y)
+
+plotData[,myShape := 1]
+plotData[scan==4,myShape := myShape+1]
+plotData[,myShape := as.factor(myShape)]
+
+#' # Do plotting ####
+#' ***
+ggp1  = ggplot(plotData, aes(x=ga, y=efwcomb/1000,  
+                             group=POPSID,shape=myShape)) +
+  #facet_wrap(~pn_emcsall,scales = "free_y")+
+  geom_line(aes(alpha=0.01,col=as.factor(pn_emcsall))) + 
+  geom_point(aes(colour = "black",fill = as.factor(pn_emcsall)))+
+  labs(x="Gestational Week", y="Weight (in kg)", 
+       fill="eCS?",shape="",color="eCS?") +
+  scale_shape_manual(values=c(21,24),
+                     labels = c("estimated \nfetal weight","birth weight"))+
+  scale_fill_manual(values = c("steelblue","darkred"),
+                    labels = c("no", "yes"))+
+  scale_colour_manual(values = c("steelblue","darkred","black"),
+                      labels = c("no", "yes","idk"))+
+  theme(legend.position = "none") + theme_classic()+ 
+  guides(alpha="none",fill="none")
+ggp1
+
+#' I really dont know how to get rid of the black color.
+#' 
+png(file=paste0("../results/_figures/05_3_Trajectory_EFW.png"),
+    width=1500,height=1200,res = 200)
+print(ggp1)
+dev.off()
+
+#' # Session Info ####
+#' ***
+sessionInfo()
+message("\nTOTAL TIME : " ,round(difftime(Sys.time(),time0,units = "mins"),3)," minutes")

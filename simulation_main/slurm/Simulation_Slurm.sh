c69361d3213fc8de5eca0018c319e4748d45e76e
pottj <pott.ne.ja@gmail.com>
2024-06-07 15:49:57 +0100

    update main simulation

--- simulation_main/slurm/Simulation_Slurm.sh
+++ simulation_main/slurm/Simulation_Slurm.sh
@@ -13,7 +13,7 @@
 ## Enter the wall-clock time limit for your jobs.
 ## If jobs reach this limit they are automatically killed.
 ## Maximum value 36:00:00.
-#SBATCH --time=06:00:00
+#SBATCH --time=08:00:00
 
 ## For single-core jobs, this number should be '1'. 
 ## If your job has built-in parallelism, eg using OpenMP or 

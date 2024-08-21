b0ca97b013878da2b79f2a3a827a3a147bdbf272
pottj <pott.ne.ja@gmail.com>
2024-08-21 17:31:55 +0100

    add real data stuff for total cholesterol

---
+++ realdata_TC/scripts/01_Prep_04_checkCADSummaryStats.R
@@ -0,0 +1,199 @@
+#' ---
+#' title: "Check outcome data"
+#' subtitle: "Longitudinal MR"
+#' author: "Janne Pott"
+#' date: "Last compiled on `r format(Sys.time(), '%d %B, %Y')`"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Introduction ####
+#' ***
+#' I want to test CAD (sex-combined, data taken from Aragam et al. (2022)) and coronary atherosclerosis (sex-stratified, Neale lab (2021?)) as outcomes. 
+#' 
+#' Here, I load the data and reduce them to the IVs for HR. I check the alleles and allele frequency. Every base position should be in hg19 / GRCh37 (our BSU UKB data is in hg19, Aragam data was downloaded using the GRCh37 build, Neale lab is using UKB, again hg19).  
+#' 
+#' - Neale lab: Variant identifier in the form "chr:pos:ref:alt", where "ref" is aligned to the forward strand of GRCh37 and "alt" is the effect allele (use this to join with variant annotation file).
+#' - Plink: ALT is effect allele
+#' 
+#' **ALT == EA, REF == OA** in both Neale and my data. Aragam uses explicitly *effect_allele* as column name. 
+#' 
+#' # Init ####
+#' ***
+rm(list = ls())
+time0<-Sys.time()
+
+source("../../SourceFile_HPC.R")
+.libPaths()
+
+#' # Load HR SNP data ####
+#' ***
+#' I need the 151 SNPs, and the four data sets.
+#' 
+load(paste0(UKB_phenotypes_filtered,"/01_Prep_03_SNPData_filtered.RData"))
+pvar2[,chrPosEAOA := paste(CHR,POS,ALT,REF,sep=":")]
+pvar2[,chrPosOAEA := paste(CHR,POS,REF,ALT,sep=":")]
+
+#' # Load UKB CAD sex-combined ####
+#' ***
+pathData = gsub("~","../../../",pathData)
+UKB_CAD_comb = fread(paste0(pathData,"I9_CORATHER.gwas.imputed_v3.both_sexes.tsv.bgz"))
+UKB_CAD_comb
+table(is.element(pvar2$chrPosEAOA,UKB_CAD_comb$variant))
+table(is.element(pvar2$chrPosOAEA,UKB_CAD_comb$variant))
+
+UKB_CAD_comb = UKB_CAD_comb[variant %in% pvar2$chrPosEAOA]
+pvar3 = copy(pvar2)
+pvar3 = pvar3[chrPosEAOA %in% UKB_CAD_comb$variant]
+
+UKB_CAD_comb[,EA := gsub(".*:","",variant)]
+UKB_CAD_comb[,OA := gsub("[0123456789]*:","",variant)]
+UKB_CAD_comb[,OA := substr(OA,1,1)]
+UKB_CAD_comb[,table(OA == EA)]
+UKB_CAD_comb[EA == minor_allele,EAF := minor_AF]
+UKB_CAD_comb[EA != minor_allele,EAF := 1-minor_AF]
+
+table(pvar3$chrPosEAOA == UKB_CAD_comb$variant)
+plot(pvar3$EAF,UKB_CAD_comb$EAF)
+plot(pvar3$MAF,UKB_CAD_comb$minor_AF)
+
+UKB_CAD_comb[, EAF2 :=1-EAF]
+UKB_CAD_comb[, beta2 := beta*(-1)]
+UKB_CAD_comb[, tstat2 := tstat*(-1)]
+plot(pvar3$EAF,UKB_CAD_comb$EAF2)
+
+UKB_CAD_comb[,rsID := pvar3$ID]
+UKB_CAD_comb[,phenotype := "UKB_CAD_combined"]
+
+#' which columns do I want? 
+names(UKB_CAD_comb)
+myAssocs_Y1 = copy(UKB_CAD_comb)
+myAssocs_Y1 = myAssocs_Y1[,c(1,19,13,14,16,20,6,17,10,18,12)]
+names(myAssocs_Y1) = c("SNP","rsID","EA","OA","EAF","phenotype","sampleSize","beta_mean","SE_mean","tval_mean","pval_mean") 
+
+#' # Load UKB CAD females ####
+#' ***
+UKB_CAD_females = fread(paste0(pathData,"I9_CORATHER.gwas.imputed_v3.female.tsv.bgz"))
+UKB_CAD_females
+table(is.element(pvar2$chrPosEAOA,UKB_CAD_females$variant))
+table(is.element(pvar2$chrPosOAEA,UKB_CAD_females$variant))
+
+UKB_CAD_females = UKB_CAD_females[variant %in% pvar2$chrPosEAOA]
+pvar3 = copy(pvar2)
+pvar3 = pvar3[chrPosEAOA %in% UKB_CAD_females$variant]
+
+UKB_CAD_females[,EA := gsub(".*:","",variant)]
+UKB_CAD_females[,OA := gsub("[0123456789]*:","",variant)]
+UKB_CAD_females[,OA := substr(OA,1,1)]
+UKB_CAD_females[,table(OA == EA)]
+UKB_CAD_females[EA == minor_allele,EAF := minor_AF]
+UKB_CAD_females[EA != minor_allele,EAF := 1-minor_AF]
+
+table(pvar3$chrPosEAOA == UKB_CAD_females$variant)
+plot(pvar3$EAF,UKB_CAD_females$EAF)
+plot(pvar3$MAF,UKB_CAD_females$minor_AF)
+
+UKB_CAD_females[, EAF2 :=1-EAF]
+UKB_CAD_females[, beta2 := beta*(-1)]
+UKB_CAD_females[, tstat2 := tstat*(-1)]
+plot(pvar3$EAF,UKB_CAD_females$EAF2)
+
+UKB_CAD_females[,rsID := pvar3$ID]
+UKB_CAD_females[,phenotype := "UKB_CAD_females"]
+
+#' which columns do I want? 
+names(UKB_CAD_females)
+myAssocs_Y2 = copy(UKB_CAD_females)
+myAssocs_Y2 = myAssocs_Y2[,c(1,19,13,14,16,20,6,17,10,18,12)]
+names(myAssocs_Y2) = c("SNP","rsID","EA","OA","EAF","phenotype","sampleSize","beta_mean","SE_mean","tval_mean","pval_mean") 
+
+#' # Load UKB CAD males ####
+#' ***
+UKB_CAD_males = fread(paste0(pathData,"I9_CORATHER.gwas.imputed_v3.male.tsv.bgz"))
+UKB_CAD_males
+table(is.element(pvar2$chrPosEAOA,UKB_CAD_males$variant))
+table(is.element(pvar2$chrPosOAEA,UKB_CAD_males$variant))
+
+UKB_CAD_males = UKB_CAD_males[variant %in% pvar2$chrPosEAOA]
+pvar3 = copy(pvar2)
+pvar3 = pvar3[chrPosEAOA %in% UKB_CAD_males$variant]
+
+UKB_CAD_males[,EA := gsub(".*:","",variant)]
+UKB_CAD_males[,OA := gsub("[0123456789]*:","",variant)]
+UKB_CAD_males[,OA := substr(OA,1,1)]
+UKB_CAD_males[,table(OA == EA)]
+UKB_CAD_males[EA == minor_allele,EAF := minor_AF]
+UKB_CAD_males[EA != minor_allele,EAF := 1-minor_AF]
+
+table(pvar3$chrPosEAOA == UKB_CAD_males$variant)
+plot(pvar3$EAF,UKB_CAD_males$EAF)
+plot(pvar3$MAF,UKB_CAD_males$minor_AF)
+
+UKB_CAD_males[, EAF2 :=1-EAF]
+UKB_CAD_males[, beta2 := beta*(-1)]
+UKB_CAD_males[, tstat2 := tstat*(-1)]
+plot(pvar3$EAF,UKB_CAD_males$EAF2)
+
+UKB_CAD_males[,rsID := pvar3$ID]
+UKB_CAD_males[,phenotype := "UKB_CAD_males"]
+
+#' which columns do I want? 
+names(UKB_CAD_males)
+myAssocs_Y3 = copy(UKB_CAD_males)
+myAssocs_Y3 = myAssocs_Y3[,c(1,19,13,14,16,20,6,17,10,18,12)]
+names(myAssocs_Y3) = c("SNP","rsID","EA","OA","EAF","phenotype","sampleSize","beta_mean","SE_mean","tval_mean","pval_mean") 
+
+#' # Load Aragam CAD sex-combined ####
+#' ***
+CADdata = fread(paste0(pathData,"GCST90132314_buildGRCh37.tsv"))
+CADdata
+pvar2[,chrPosEAOA2 := paste0(CHR,":",POS,"_",ALT,"_",REF)]
+pvar2[,chrPosOAEA2 := paste0(CHR,":",POS,"_",REF,"_",ALT)]
+table(is.element(pvar2$chrPosEAOA2,CADdata$markername),
+      is.element(pvar2$chrPosOAEA2,CADdata$markername))
+
+CADdata = CADdata[markername %in% pvar2$chrPosEAOA2 | markername %in% pvar2$chrPosOAEA2]
+pvar3 = copy(pvar2)
+pvar3 = pvar3[chrPosEAOA2 %in% CADdata$markername | chrPosOAEA2 %in% CADdata$markername]
+
+CADdata[,EA := toupper(effect_allele)]
+CADdata[,OA := toupper(other_allele)]
+CADdata[,table(OA == EA)]
+CADdata[,EAF := effect_allele_frequency]
+
+table(pvar3$chrPosEAOA2 == CADdata$markername)
+table(pvar3$chrPosOAEA2 == CADdata$markername)
+filt = pvar3$ALT == CADdata$EA
+plot(pvar3$EAF[filt],CADdata$EAF[filt])
+plot(pvar3$EAF[!filt],CADdata$EAF[!filt])
+
+CADdata[!filt, EAF :=1-EAF]
+CADdata[!filt, beta := beta*(-1)]
+plot(pvar3$EAF,CADdata$EAF)
+
+CADdata[,rsID := pvar3$ID]
+CADdata[,phenotype := "Agaram_CAD_combined"]
+CADdata[,tval := beta/standard_error]
+
+#' which columns do I want? 
+names(CADdata)
+myAssocs_Y0 = copy(CADdata)
+myAssocs_Y0 = myAssocs_Y0[,c(10,26,23,24,25,27,21,8,9,28,1)]
+names(myAssocs_Y0) = c("SNP","rsID","EA","OA","EAF","phenotype","sampleSize","beta_mean","SE_mean","tval_mean","pval_mean") 
+myAssocs_Y0[,SNP := pvar3$chrPosEAOA]
+
+
+#' ### Combine data
+myAssocs_Y = rbind(myAssocs_Y0,myAssocs_Y1,myAssocs_Y2,myAssocs_Y3)
+
+#' ## save as temporary files
+save(myAssocs_Y, file = paste0("../results/01_Prep_04_CADsummaryStats.RData"))
+
+#' # Session Info ####
+#' ***
+sessionInfo()
+message("\nTOTAL TIME : " ,round(difftime(Sys.time(),time0,units = "mins"),3)," minutes")
---
+++ realdata_TC/scripts/02_SNPs_05_SENS_SNPset.R
@@ -0,0 +1,120 @@
+#' ---
+#' title: "Get association of TC SNPs on exposure"
+#' subtitle: "Longitudinal MVMR"
+#' author: "Janne Pott"
+#' date: "Last compiled on `r format(Sys.time(), '%d %B, %Y')`"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Introduction ####
+#' ***
+#' Here, I want to estimate the SNP effects in a **gamlssIA** model for TC. 
+#' 
+#' I will use the minimal model for the constant stage (adjust for age, sex, and phaseTime). In addition, the main model will only include a random intercept. Random slope will be added in the sensitivity checks. 
+#' 
+#' # Initialize ####
+#' ***
+rm(list = ls())
+time0<-Sys.time()
+
+source("../../SourceFile_HPC.R")
+.libPaths()
+
+#' # Load and prep UKB data ####
+#' ***
+#' Load data (genetic data & phenotype data)
+load(paste0(UKB_phenotypes_filtered,"/01_Prep_01_UKB_GP_TC.RData"))
+load(paste0(UKB_phenotypes_filtered,"/01_Prep_06_SNPData_filtered_sens.RData"))
+
+stopifnot(is.element(myTab7$ID,psam$FID))
+stopifnot(pvar2$ID == colnames(geno_mat2))
+stopifnot(psam$FID == rownames(geno_mat2))
+pvar2[,rsID := ID]
+
+#' # Get effects ####
+#' ***
+matched = match(myTab6$BSU_ID,myTab7$ID)
+myTab_long = cbind(myTab6,myTab7[matched,c(2,7:16)])
+myTab_long[sex==0,sex:=2]
+myTab_long[,exposure_type := NULL]
+
+data1 = copy(myTab_long)
+setnames(data1,"exposure_value","TC")
+setnames(data1,"BSU_ID","ID")
+names(data1)[11:20] = gsub("_","",names(data1)[11:20])
+
+# prepare loop over SNPs
+mySNPs = pvar2$ID
+matched = match(data1$ID,psam$FID)
+table(is.na(matched))
+
+registerDoParallel(as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK")))
+
+dumTab2 = foreach(i = 1:length(mySNPs))%dorng%{
+  #dumTab2 = foreach(i = 1:length(mySNPs))%do%{
+  #i=1
+  mySNP = geno_mat2[,i]
+  
+  data2 = copy(data1)
+  data2[,myG := mySNP[matched]]
+  
+  mod2 = gamlss(TC ~ sex + myG*exposure_age + 
+                  PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + random(x = as.factor(ID)),   
+                sigma.formula = ~ myG + sex + exposure_age, 
+                data = na.omit(data2), family = "NO")
+  
+  data3 = copy(data2)
+  data3 = data3[sex == 1,]
+  mod2M = gamlss(TC ~ myG*exposure_age +  
+                   PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + random(x = as.factor(ID)),   
+                 sigma.formula = ~ myG + exposure_age, 
+                 data = na.omit(data3), family = "NO")
+  
+  data4 = copy(data2)
+  data4 = data4[sex == 2,]
+  mod2F = gamlss(TC ~ myG*exposure_age + 
+                   PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + random(x = as.factor(ID)),   
+                 sigma.formula = ~ myG + exposure_age, 
+                 data = na.omit(data4), family = "NO")
+  
+  dummy2 = summary(mod2)
+  dummy2 = dummy2[grepl("myG",rownames(dummy2)),]
+  dummy2M = summary(mod2M)
+  dummy2M = dummy2M[grepl("myG",rownames(dummy2M)),]
+  dummy2F = summary(mod2F)
+  dummy2F = dummy2F[grepl("myG",rownames(dummy2F)),]
+  
+  n1= length(unique(data2[,ID]))
+  n2= length(unique(data3[,ID]))
+  n3= length(unique(data4[,ID]))
+  res1 = data.table(SNP = rep(pvar2[i,ID],3),
+                    model = c("combined","men","women"),
+                    sampleSize = c(n1,n2,n3),
+                    beta_mean = c(dummy2[1,1],dummy2M[1,1],dummy2F[1,1]),
+                    SE_mean =   c(dummy2[1,2],dummy2M[1,2],dummy2F[1,2]),
+                    tval_mean = c(dummy2[1,3],dummy2M[1,3],dummy2F[1,3]),
+                    pval_mean = c(dummy2[1,4],dummy2M[1,4],dummy2F[1,4]),
+                    beta_slope = c(dummy2[2,1],dummy2M[2,1],dummy2F[2,1]),
+                    SE_slope =   c(dummy2[2,2],dummy2M[2,2],dummy2F[2,2]),
+                    tval_slope = c(dummy2[2,3],dummy2M[2,3],dummy2F[2,3]),
+                    pval_slope = c(dummy2[2,4],dummy2M[2,4],dummy2F[2,4]),
+                    beta_var = c(dummy2[3,1],dummy2M[3,1],dummy2F[3,1]),
+                    SE_var =   c(dummy2[3,2],dummy2M[3,2],dummy2F[3,2]),
+                    tval_var = c(dummy2[3,3],dummy2M[3,3],dummy2F[3,3]),
+                    pval_var = c(dummy2[3,4],dummy2M[3,4],dummy2F[3,4]))
+  res1
+}
+myAssocs_X = rbindlist(dumTab2)
+myAssocs_X
+
+save(myAssocs_X,file=paste0("../results/02_SNPs_05_SENS_SNPset.RData"))
+
+#' # Session Info ####
+#' ***
+sessionInfo()
+message("\nTOTAL TIME : " ,round(difftime(Sys.time(),time0,units = "mins"),3)," minutes")
